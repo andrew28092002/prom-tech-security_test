ee97aa90ecc2aae15e526cf498b05a8fec73e1d6
andrew28092002 <andreiastanaev2002@gmail.com>
2023-03-13 16:43:13 +0500

    some styles

--- src/app/component/item/item.component.ts
+++ src/app/component/item/item.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TItem } from 'src/assets/types/item';
+import { genre } from 'src/assets/data/genre';
+import { MatIconButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.scss']
+  styleUrls: ['./item.component.scss'],
 })
 export class ItemComponent {
+  @Input('item') item: TItem;
+  genre: Map<number, string> = genre;
 
+  reverseGenre() {
+    return [...this.item.genre].map((num) => this.genre.get(num)).join(', ');
+  }
+
+  onMouseEnter(button: MatIconButton) {
+    button._elementRef.nativeElement.style.opacity = '1'
+  }
+
+  onMouseLeave(button: MatIconButton) {
+    button._elementRef.nativeElement.style.opacity = '0'
+  }
 }
---
+++ src/assets/types/item.ts
@@ -0,0 +1,7 @@
+export type TItem = {
+    id: number,
+    name: string,
+    year: number,
+    description: string,
+    genre: number[]
+}
