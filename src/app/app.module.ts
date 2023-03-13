112a813ba4a2fcabe92018def7c4820bd067c15b
andrew28092002 <andreiastanaev2002@gmail.com>
2023-03-13 13:18:34 +0500

    delete app-default html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,36 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button'; 
-import {MatIconModule} from '@angular/material/icon'; 
-import {MatInputModule} from '@angular/material/input'; 
-import {MatSelectModule} from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { ItemsComponent } from './component/items/items.component';
 import { ItemComponent } from './component/item/item.component';
 import { ModalComponent } from './component/modal/modal.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ItemsComponent,
-    ItemComponent,
-    ModalComponent,
-  ],
+  declarations: [AppComponent, ItemsComponent, ItemComponent, ModalComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    FormsModule,
     MatCardModule,
     MatButtonModule,
     MatIconModule,
     MatInputModule,
-    MatSelectModule
+    MatSelectModule,
+    MatFormFieldModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
